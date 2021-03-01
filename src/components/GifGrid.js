08f470d88fa385886cb8fdb189479d30b8774e56
Frandpj <fdelpinojimenez@gmail.com>
2021-03-02 00:46:30 +0100

    useFetchGifs - obtener imágenes y bandera de carga

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import { useFetchGifs } from '../hooks/useFetchGifs'
-// import { GifGridItem } from './GifGridItem';
-// import { getGifs } from '../helpers/getGifs';
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
-    // const [images, setImages] = useState([])
-    const { loading } = useFetchGifs();
+    /*
+    Al desestructurar se le puede cambiar el nombre a la variable
+    con ":", ejemplo: data:images, ahora data se llama images
+    */
+    const { data:images, loading } = useFetchGifs(category);
 
     console.log(loading);
 
-    /* useEffect con un segundo argumento array vacío hace que este código solo 
+    /*
+    useEffect con un segundo argumento array vacío hace que este código solo 
     se ejecute una vez, si en el array añadimos el parámetro, si por algún casual
-    cambia la categoría (parámetro) volverá a disparar la función */
+    cambia la categoría (parámetro) volverá a disparar la función 
+    */
     // useEffect(() => {
     //     getGifs(category)
     //         .then(setImages)
@@ -21,10 +25,11 @@ export const GifGrid = ({ category }) => {
     return (
         <>
             <h3>{ category }</h3>
+            {/* Forma más corta de un if else, ya que solo se pone el if si sale bien */}
+            {loading && <p>Loading</p>}
 
-            { loading  ? 'Cargando...' : 'Data cargada'}
-            {/* <div className="card-grid">
-                Tarea, imprimir lista con key = id y el titulo = title de la función getGifs 
+            <div className="card-grid">
+                {/* Tarea, imprimir lista con key = id y el titulo = title de la función getGifs  */}
                 {
                     images.map(img => (
                         <GifGridItem 
@@ -35,7 +40,7 @@ export const GifGrid = ({ category }) => {
                         />
                     ))
                 }
-            </div> */}
+            </div>
         </>
     )
 }
--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,19 +1,34 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { getGifs } from "../helpers/getGifs";
 
-export const useFetchGifs = () => {
+export const useFetchGifs = (category) => {
     
     const [state, setState] = useState({
         data: [],
         loading: true
     });
 
+    useEffect(() => {
+
+        getGifs(category)
+            .then(imgs => {
+                setTimeout(() => {
+                    setState({
+                        data: imgs,
+                        loading: false
+                    });
+                }, 3000);
+        });
+
+    }, [category]);
+
     // A los 3 segundos se cargan los datos y deja de cargar (loading: false)
-    setTimeout(() => {
-        setState({
-            data: [1, 2, 3, 4, 5, 6, 7],
-            loading: false
-        })
-    }, 3000);
+    // setTimeout(() => {
+    //     setState({
+    //         data: [1, 2, 3, 4, 5, 6, 7],
+    //         loading: false
+    //     })
+    // }, 3000);
 
     return state; // { data: [] , loading: true};
 
